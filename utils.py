d096e6e356435200abd257d87959ef568c07fdb8
gheorghe.grosu <gheorghe.grosu@stud.acs.upb.ro>
2024-04-24 22:11:20 +0300

    added parse_constraints from yaml data in utils

--- utils.py
+++ utils.py
@@ -9,9 +9,6 @@ MATERII = 'Materii'
 PROFESORI = 'Profesori'
 SALI = 'Sali'
 
-
-##################### FUNCTII #####################
-
 def read_yaml_file(file_path : str) -> dict:
     '''
     Citeste un fișier yaml și returnează conținutul său sub formă de dicționar
@@ -20,7 +17,7 @@ def read_yaml_file(file_path : str) -> dict:
         return yaml.safe_load(file)
 
 
-def example_yaml_attributes_access(yaml_dict : dict):
+def acces_yaml_attributes(yaml_dict : dict):
     '''
     Primește un dicționar yaml și afișează datele referitoare la atributele sale
     '''
@@ -38,28 +35,33 @@ def example_yaml_attributes_access(yaml_dict : dict):
     print(*list(yaml_dict[SALI].keys()), sep=', ')
 
 
-
-def prof_initials(prof : str, profs_to_initials : {str: str}, initials_count: {str: int}) -> str:
+def get_profs_initials(profs : list) -> dict:
     '''
-    Funcție auxiliară pentru pretty_print_timetable
+    Primește o listă de profesori
 
-    Pentru un profesor dat, returnează inițialele sale, adăugând un număr la final în cazul în care aceste inițiale sunt deja folosite
-
-    !RECOMANDĂM SĂ NU FOLOSIȚI ACEASTĂ FUNCȚIE ÎN RECURENȚĂ, DEOARECE ADĂUGAȚI TIMP EXECUȚIE UNEI PROBLEME CU SPAȚIU DE CĂUTARE MARE!
+    Returnează două dicționare:
+    - unul care are numele profesorilor drept chei și drept valori prescurtările lor (prof_to_initials[prof] = initiale)
+    - unul care are prescurtările profesorilor drept chei și drept valori numele lor (initials_to_prof[initiale] = prof)
     '''
 
-    if prof in profs_to_initials:
-        return profs_to_initials[prof]
-    else:
-        initials = prof[0] + prof.split(' ')[1][0]
+    initials_to_prof = {}
+    prof_to_initials = {}
+    initials_count = {}
+
+    for prof in profs:
+        name_components = prof.split(' ')
+        initials = name_components[0][0] + name_components[1][0]
+        
         if initials in initials_count:
             initials_count[initials] += 1
             initials += str(initials_count[initials])
         else:
             initials_count[initials] = 1
-            
-        profs_to_initials[prof] = initials
-        return initials
+        
+        initials_to_prof[initials] = prof
+        prof_to_initials[prof] = initials
+        
+    return prof_to_initials, initials_to_prof
 
 
 def allign_string_with_spaces(s : str, max_len : int, allignment_type : str = 'center') -> str:
@@ -87,7 +89,7 @@ def allign_string_with_spaces(s : str, max_len : int, allignment_type : str = 'c
     return s
 
 
-def pretty_print_timetable_aux_zile(timetable : {str : {(int, int) : {str : (str, str)}}}) -> str:
+def pretty_print_timetable_aux_zile(timetable : {str : {(int, int) : {str : (str, str)}}}, input_path : str) -> str:
     '''
     Primește un dicționar ce are chei zilele, cu valori dicționare de intervale reprezentate ca tupluri de int-uri, cu valori dicționare de săli, cu valori tupluri (profesor, materie)
 
@@ -96,7 +98,8 @@ def pretty_print_timetable_aux_zile(timetable : {str : {(int, int) : {str : (str
 
     max_len = 30
 
-    profs_to_initials, initials_count = {}, {}
+    profs = read_yaml_file(input_path)[PROFESORI].keys()
+    profs_to_initials, _ = get_profs_initials(profs)
 
     table_str = '|           Interval           |             Luni             |             Marti            |           Miercuri           |              Joi             |            Vineri            |\n'
 
@@ -127,14 +130,14 @@ def pretty_print_timetable_aux_zile(timetable : {str : {(int, int) : {str : (str
                     s_interval += allign_string_with_spaces(f'{classroom} - goala', max_len, 'left')
                 else:
                     prof, subject = classes[classroom]
-                    s_interval += allign_string_with_spaces(f'{subject} : ({classroom} - {prof_initials(prof, profs_to_initials, initials_count)})', max_len, 'left')
+                    s_interval += allign_string_with_spaces(f'{subject} : ({classroom} - {profs_to_initials[prof]})', max_len, 'left')
             
             s_interval += '|\n'
         table_str += s_interval + delim
 
     return table_str
 
-def pretty_print_timetable_aux_intervale(timetable : {(int, int) : {str : {str : (str, str)}}}) -> str:
+def pretty_print_timetable_aux_intervale(timetable : {(int, int) : {str : {str : (str, str)}}}, input_path : str) -> str:
     '''
     Primește un dicționar de intervale reprezentate ca tupluri de int-uri, cu valori dicționare de zile, cu valori dicționare de săli, cu valori tupluri (profesor, materie)
 
@@ -143,7 +146,8 @@ def pretty_print_timetable_aux_intervale(timetable : {(int, int) : {str : {str :
 
     max_len = 30
 
-    profs_to_initials, initials_count = {}, {}
+    profs = read_yaml_file(input_path)[PROFESORI].keys()
+    profs_to_initials, _ = get_profs_initials(profs)
 
     table_str = '|           Interval           |             Luni             |             Marti            |           Miercuri           |              Joi             |            Vineri            |\n'
 
@@ -170,14 +174,14 @@ def pretty_print_timetable_aux_intervale(timetable : {(int, int) : {str : {str :
                     s_interval += allign_string_with_spaces(f'{classroom} - goala', max_len, 'left')
                 else:
                     prof, subject = classes[classroom]
-                    s_interval += allign_string_with_spaces(f'{subject} : ({classroom} - {prof_initials(prof, profs_to_initials, initials_count)})', max_len, 'left')
+                    s_interval += allign_string_with_spaces(f'{subject} : ({classroom} - {profs_to_initials[prof]})', max_len, 'left')
             
             s_interval += '|\n'
         table_str += s_interval + delim
 
     return table_str
 
-def pretty_print_timetable(timetable : dict) -> str:
+def pretty_print_timetable(timetable : dict, input_path : str) -> str:
     '''
     Poate primi fie un dictionar de zile conținând dicționare de intervale conținând dicționare de săli cu tupluri (profesor, materie)
     fie un dictionar de intervale conținând dictionare de zile conținând dicționare de săli cu tupluri (profesor, materie)
@@ -185,16 +189,124 @@ def pretty_print_timetable(timetable : dict) -> str:
     Pentru cazul în care o sală nu este ocupată la un moment de timp, se așteaptă 'None' în valoare, în loc de tuplu
     '''
     if 'Luni' in timetable:
-        return pretty_print_timetable_aux_zile(timetable)
+        return pretty_print_timetable_aux_zile(timetable, input_path)
     else:
-        return pretty_print_timetable_aux_intervale(timetable)
+        return pretty_print_timetable_aux_intervale(timetable, input_path)
+
+def parse_constraints(yaml_data):
+    """
+    Parse the YAML data and extract constraints into dictionaries.
+    """
+    # Extract intervals, days, subjects, professors, and rooms
+    intervals = yaml_data[INTERVALE]
+    days = yaml_data[ZILE]
+    subjects = yaml_data[MATERII]
+    professors = yaml_data[PROFESORI]
+    rooms = yaml_data[SALI]
+
+    # Initialize dictionaries for constraints
+    teacher_availability = {}
+    room_capacity_and_subjects = {}
+    professor_preferred = {}
+
+    # Parse professor constraints
+    for professor, info in professors.items():
+        preferred = {}
+        availability = {}
+
+        for constraint in info['Constrangeri']:
+            if isinstance(constraint, str):
+                if constraint in days or constraint[1:] in days:
+                    if constraint.startswith('!'):
+                        day = constraint[1:]
+                    else:
+                        day = constraint
+                        preferred[day] = []
+                    availability[day] = []
+                else:
+                    if constraint.startswith('!'):
+                        interval = constraint[1:]
+                    else:
+                        interval = constraint
+                        for day in preferred:
+                            preferred[day].append(interval)
+                    for day in availability:
+                        availability[day].append(interval)
+
+        # Split intervals with differences more than 2 hours
+        split_intervals(preferred)
+        split_intervals(availability)
+
+        professor_preferred[professor] = preferred
+        teacher_availability[professor] = availability
+
+    # Parse room capacities and subjects
+    for room, info in rooms.items():
+        room_capacity_and_subjects[room] = (info['Capacitate'], info['Materii'])
+
+    return intervals, days, subjects, teacher_availability, room_capacity_and_subjects, professor_preferred
+
+
+def split_intervals(non_preferred):
+    for day in non_preferred:
+        updated_intervals = []
+        for interval in non_preferred[day]:
+            start, end = map(int, interval.split('-'))
+            if end - start > 2:
+                # Split the interval into two-hour intervals
+                for i in range(start, end, 2):
+                    updated_intervals.append((i, i + 2))
+            else:
+                updated_intervals.append((start, end))
+        non_preferred[day] = updated_intervals
 
 
 if __name__ == '__main__':
-    filename = f'inputs/orar_mic_exact.yaml'
+    import numpy as np
 
-    timetable_specs = read_yaml_file(filename)
+    # Definirea datelor din orar
+    days = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri']
+    intervals = [(8, 10), (10, 12), (12, 14), (14, 16), (16, 18), (18, 20)]
+    subjects = {'PA': 330, 'PCom': 330, 'PL': 300}
 
-    example_yaml_attributes_access(timetable_specs)
+    # Definirea tabloului NumPy
+    timetable_np = np.empty((len(days), len(intervals)), dtype=object)
 
-    # print(pretty_print_timetable(timetable))
+    # Umplerea tabloului cu datele orarului
+    for i, day in enumerate(days):
+        for j, interval in enumerate(intervals):
+            timetable_np[i, j] = {
+                'profesor': 'Nume profesor',
+                'materie': 'Nume materie'
+                # alte informații relevante pentru intervalul de timp
+            }
+
+    # Afișarea tabloului NumPy
+    print(timetable_np)
+
+    # Apelul functiei pretty_print_timetable
+
+#     filename = f'inputs/orar_mic_exact.yaml'
+#
+    # timetable_specs = read_yaml_file(filename)
+#
+#     acces_yaml_attributes(timetable_specs)
+#
+#     (intervals, days, subjects, teacher_availability, room_capacity_and_subjects,
+#      professor_preferred) = parse_constraints(timetable_specs)
+#
+#     print('Intervalele sunt:', intervals)
+#     print()
+#     print('Zilele sunt:', days)
+#     print()
+#     print('Materiile sunt:', subjects)
+#     print()
+#     print('Disponibilitatea profesorilor este:', teacher_availability)
+#     print()
+#     print('Capacitatile salilor sunt:', room_capacity_and_subjects)
+#     print()
+#     print('Preferintele profesorilor sunt:', professor_preferred)
+
+
+
+    # print(pretty_print_timetable(timetable, filename))
